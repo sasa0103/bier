f5af2406c59e405469649998a29c42262af5aeef
Sarah Dossinger <s_dossinge10@cs.uni-kl.de>
2015-11-28 21:13:29 +0100

    fix things

--- source/javascripts/rating.js
+++ source/javascripts/rating.js
@@ -47,27 +47,27 @@ function changeStars(n, r) {
 	switch (r) {
 	    case 1: $(':radio[name=rating_' + n + ']:nth(0)').attr("checked", true);
 	    		$('#rated_' + n ).text("");
-	    		$('#rated_' + n ).closest().css({'background-color':'white'});
+	    		$('#rated_' + n ).closest('article').css({'background-color':'white'});
 	    		break;
 	    case 2: $(':radio[name=rating_' + n + ']:nth(1)').attr("checked", true);
 	    		$('#rated_' + n ).text("");
-	    		$('#rated_' + n ).closest().css({'background-color':'white'});
+	    		$('#rated_' + n ).closest('article').css({'background-color':'white'});
 	    		break;
 	    case 3: $(':radio[name=rating_' + n + ']:nth(2)').attr("checked", true);
 	    		$('#rated_' + n ).text("");
-	    		$('#rated_' + n ).closest().css({'background-color':'white'});
+	    		$('#rated_' + n ).closest('article').css({'background-color':'white'});
 	    		break;
 	    case 4: $(':radio[name=rating_' + n + ']:nth(3)').attr("checked", true);
 	    		$('#rated_' + n ).text("");
-	    		$('#rated_' + n ).closest().css({'background-color':'white'});
+	    		$('#rated_' + n ).closest('article').css({'background-color':'white'});
 	    		break;
 	    case 5: $(':radio[name=rating_' + n + ']:nth(4)').attr("checked", true);
 	    		$('#rated_' + n ).text("");
-	    		$('#rated_' + n ).closest().css({'background-color':'white'});
+	    		$('#rated_' + n ).closest('article').css({'background-color':'white'});
 	    		break;
 	    default: $(':radio[name=rating_' + n + ']').removeAttr('checked');
 	    		$('#rated_' + n ).text("Diese Bier hast du noch nicht bewertet!");
-	    		$('#rated_' + n ).closest().css({'background-color':'#D6C6CF'});
+	    		$('#rated_' + n ).closest('article').css({'background-color':'#D6C6CF'});
 	}
 }
 
